49dc036ef82bdf5314793abe990129141c501ce6
Adam B <adamisbarker@hotmail.com>
2020-11-28 23:11:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(){
-  var food = ['pizza', 'nachos', 'chips']
+  var food = ['pizza']
   return ['foo', ...food]
 }
 
 function destructivelyAddElementToBeginningOfArray(){
-  var food = ['pizza', 'nachos', 'chips']
+  var food = ['pizza']
   food.unshift ('foo')
   return food
 }
